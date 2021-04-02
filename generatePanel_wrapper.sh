94c075e2ba158ad6ff78eb59108baa6e3d6628f7
Ellen Schofield <ellen@pawprintsdesign.co.uk>
2021-04-02 16:31:16 +0100

    added ability to loop around panels

--- generatePanel_wrapper.sh
+++ generatePanel_wrapper.sh
@@ -2,31 +2,40 @@
 
 #! RUN : bash generatePanel_wrapper.sh <TAG> <CHR>
 
-TAG=$1
-CHR=$2
+CHR=$1
+PANELS=$2
 
-[[ -z "$TAG" ]] && { echo "ERROR: No TAG given for this run"; exit 1; }
 [[ -z "$CHR" ]] && { echo "ERROR: No CHR provided for this run"; exit 1; }
+[[ -z "$PANELS" ]] && { echo "ERROR: No PANELS provided for this run"; exit 1; }
 
-source ${TAG}.config
+JOBS=""
 
-mkdir -p "$ID/logs"; cd $ID
-cp ../${TAG}.config .
+for TAG in `echo $PANELS | tr ':' "\n"`; do
+  source ${TAG}.config
+
+  mkdir -p "$TAG/logs"; cd $TAG
+  cp ../${TAG}.config .
 
 
-# prepare files
-# Take VCF value and look to see if need to make CHR specific VCF file
+  # prepare files
+  # Take VCF value and look to see if need to make CHR specific VCF file
+  if [[ ! -e $VCF_ALL && -e $VCF ]]
+  then 
+    echo "ERROR - Unable to find VCF file for ${TAG}";
+    exit 1;
+  fi
 
-if [[ ! -e $VCF_ALL && -e $VCF ]]
-then 
-  echo "ERROR - Unable to find VCF file for ${TAG}";
-  exit 1;
-fi
+  # The construct ${jid1##* } isolates the last word from "Submitted batch job XXXXXX" - returns XXXXXX
+  # vcfChr.sh
+  jid1=$(sbatch -J ${TAG}.vcfChr ${HOME}/scripts/Imputation/slurm/vcfChr.sh ${TAG} ${CHR})
+  # vcfQC.sh - convert VCF to plink; perform QC; check SNPs; convert back to VCF
+  jid2=$(sbatch -J ${TAG}.vcfQC --dependency=afterok:${jid1##* } ${HOME}/scripts/Imputation/slurm/vcfQC.sh ${TAG} ${CHR})
 
-# vcfChr.sh
-jid1=$(sbatch -J ${TAG}.vcfChr ${HOME}/scripts/Imputation/slurm/vcfChr.sh ${TAG} ${CHR})
-# vcfQC.sh - convert VCF to plink; perform QC; check SNPs; convert back to VCF
-jid2=$(sbatch -J ${TAG}.vcfQC --dependency=afterok:${jid1##* } ${HOME}/scripts/Imputation/slurm/vcfQC.sh ${TAG} ${CHR})
+  echo $jid1
+  echo $jid2
 
-# The construct ${jid1##* } isolates the last word
-echo ${jid1##* }
+  JOBS+="${jid2##* }:"
+
+done
+
+echo "${JOBS::-1}"
