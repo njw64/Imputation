0185f9c08d45372d2b124a55417d98931f38fe23
Ellen Schofield <ellen@pawprintsdesign.co.uk>
2021-04-05 12:28:50 +0100

    created script to generate chr-specific BAM files

---
+++ slurm/prepareBamFiles.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+#! RUN : sbatch prepareBamFiles.sh 
+
+#! sbatch directives begin here ###############################
+#! Which project should be charged:
+#SBATCH -A GODOGS-SL2-CPU
+#! How many whole nodes should be allocated?
+#SBATCH --nodes=1
+#! How many (MPI) tasks will there be in total? (<= nodes*32)
+#! The skylake/skylake-himem nodes have 32 CPUs (cores) each.
+#SBATCH --ntasks=1
+#! How much wallclock time will be required?
+#SBATCH --time=1:00:00
+#! What types of email messages do you wish to receive?
+#SBATCH --mail-type=ALL
+##SBATCH --mail-type=FAIL
+#! Uncomment this to prevent the job from being requeued (e.g. if
+#! interrupted by node failure or system downtime):
+##SBATCH --no-requeue
+#! For 6GB per CPU, set "-p skylake"; for 12GB per CPU, set "-p skylake-himem":
+#SBATCH -p skylake
+
+#SBATCH -o logs/job-%j.out
+
+module purge                               # Removes all modules still loaded
+module load rhel7/default-peta4            # REQUIRED - loads the basic environment
+
+module load samtools/1.9                          # samtools
+
+PROJECT=$1
+CHR=$2
+source setup.config
+
+FILE=$(ls ${BAM_FILES}*.bam | sed -n ${SLURM_ARRAY_TASK_ID}p)
+samtools view -b ${FILE} 30 > ${BAM_FILES}/chr${CHR}/chr${CHR}-$FILE
