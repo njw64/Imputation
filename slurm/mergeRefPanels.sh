ea8deacca6195ec317ced593d87151870fa61ed2
Ellen Schofield <ellen@pawprintsdesign.co.uk>
2021-04-05 11:29:49 +0100

    added section to prepare files for addRefSNPs

--- slurm/mergeRefPanels.sh
+++ slurm/mergeRefPanels.sh
@@ -27,6 +27,7 @@ module load rhel7/default-peta4            # REQUIRED - loads the basic environm
 
 module load bcftools-1.9-gcc-5.4.0-b2hdt5n        # bcftools
 module load tabix-2013-12-16-gcc-5.4.0-xn3xiv7    # bgzip/tabix
+module load bedtools/2.20.1                       # bedtools
 
 PROJECT=$1
 source setup.config
@@ -43,3 +44,12 @@ bcftools view -Ov -S ^samples.dups merged.vcf | bcftools view -m2 -M2 -v snps | 
 tabix -p vcf ${PROJECT}.ref-panel.vcf.gz
 
 rm -rf merged.vcf samples.dups
+
+# prepare for adding ref SNPs to the PROJECT VCF
+cd ${PROJECT}
+mkdir addRefSNPs; cd addRefSNPs
+# create bed file of VCF panel
+bcftools view -H ../../${PROJECT}.ref-panel.vcf.gz| awk  -v OFS='\t' '{print $1, $2-1, $2, $3}' > ../../${PROJECT}.ref-panel.snps.bed
+# create list of unique SNPs in ref panel
+subtractBed -a ../../${PROJECT}.ref-panel.snps.bed -b ../${PROJECT}.snps.bed -A > ref.uniq.snps.bed
+cd ../../
