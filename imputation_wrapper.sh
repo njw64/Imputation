25ae7219f5f02a1a998bb1dc8a8c448f379ed634
Ellen Schofield <ellen@pawprintsdesign.co.uk>
2021-04-15 10:06:06 +0100

    imputaion scripts

---
+++ imputation_wrapper.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+#! RUN : bash generatePanel_wrapper.sh <CHR> <PROJECT>
+#! Eg. : bash generatePanel_wrapper.sh 13 go_lab
+
+CHR=$1
+PROJECT=$2
+
+[[ -z "$CHR" ]] && { echo "ERROR: No CHR provided for this run"; exit 1; }
+[[ -z "$PROJECT" ]] && { echo "ERROR: No PROJECT provided for this run"; exit 1; }
+[[ ! -e chr${CHR} ]] && { echo "No directory exists for chr ${CHR}"; exit 1; }
+
+cd chr${CHR};
+
+# Check that the PROJECT haplotypes and legend files exist
+if [[ ! -e ${PROJECT}/shapeit/${PROJECT}.phased.impute.haplotypes && -e ${PROJECT}/shapeit/${PROJECT}.phased.impute.legend ]]
+then 
+  echo "ERROR - Unable to find haplotypes/legend files for ${PROJECT} - have you run generatePanel_wrapper.sh first?";
+  exit 1;
+fi
+
+# Check that the known haplotypes from teh GWAS data exist
+if [ ! -e ${GWAS_PLINK}/${GWAS_PLINK}.phased.haps ]
+then 
+  echo "ERROR - Unable to find GWAS known haplotypes for ${GWAS_PLINK} - have you run prepGwasData_wrapper.sh first?";
+  exit 1;
+fi
+
+# Files exist so we are ready to impute!
+jid1=$(sbatch -J ${GWAS_PLINK}.impute ${HOME}/scripts/Imputation/slurm/runImpute.sh ${PROJECT} ${CHR})
---
+++ slurm/runImpute.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+#! RUN : sbatch runImpute.sh 
+
+#! sbatch directives begin here ###############################
+#! Which project should be charged:
+#SBATCH -A GODOGS-SL2-CPU
+#! How many whole nodes should be allocated?
+#SBATCH --nodes=1
+#! How many (MPI) tasks will there be in total? (<= nodes*32)
+#! The skylake/skylake-himem nodes have 32 CPUs (cores) each.
+#SBATCH --ntasks=8
+#! How much wallclock time will be required?
+#SBATCH --time 12:00:00
+#! What types of email messages do you wish to receive?
+#SBATCH --mail-type=ALL
+#! Uncomment this to prevent the job from being requeued (e.g. if
+#! interrupted by node failure or system downtime):
+##SBATCH --no-requeue
+
+#! For 6GB per CPU, set "-p skylake"; for 12GB per CPU, set "-p skylake-himem":
+#SBATCH -p skylake
+
+#SBATCH -o logs/job-%j.out
+
+module purge                               # Removes all modules still loaded
+module load rhel7/default-peta4            # REQUIRED - loads the basic environment
+
+
+PROJECT=$1
+CHR=$2
+source setup.config
+
+LEN=`perl -lane 'if($F[1] eq "SN:'${CHR}'"){ $F[2] =~ s/LN://; print $F[2]; }' ${GENOME}.dict`
+HAPS_FILE="${PROJECT}/shapeit/${PROJECT}.phased.impute.haplotypes"
+LEGEND_FILE="${PROJECT}/shapeit/${PROJECT}.phased.impute.legend"
+GWAS_HAPS_FILE="${GWAS_PLINK}/${GWAS_PLINK}.phased.haps"
+
+impute2 -use_prephased_g -m ${MAPS}/chr${CHR}.cf3.1_map.txt -h ${HAPS_FILE} -l ${LEGEND_FILE} -known_haps_g ${GWAS_HAPS_FILE} -int 1 ${LEN} -allow_large_regions -Ne 200 -o ${PROJECT}.impute2 -phase
